a887bfa0c22a2d9688f016985eed45ddde26d2c9
Lyss Garcia <alyssa.g200@gmail.com>
2022-01-28 12:46:56 -0800

    added connection file

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const cwd = process.cwd();
 
-app.use(express.json());
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
-app.use(require('./routes'));
+app.use(express.json());
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server for Social Network DB running on port ${PORT}!`);
+    });
 });
-
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
